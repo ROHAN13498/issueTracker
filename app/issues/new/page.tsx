61cccd1ee46f4482fa23dd9a4925ea1209c90d87
rohan <95855114+ROHAN13498@users.noreply.github.com>
2024-01-02 12:11:12 +0530

    handled errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,18 +1,44 @@
 'use client'
+import { TextField,Button, CalloutText,Callout } from '@radix-ui/themes'
+import React, { useState } from 'react'
+import dynamic from 'next/dynamic'
+import {useForm,Controller} from 'react-hook-form'
+import "easymde/dist/easymde.min.css";
+import { useRouter } from 'next/navigation'
+import axios from 'axios'
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), { ssr: false }) 
 
-import { TextArea, TextField } from '@radix-ui/themes'
-import React from 'react'
-import { Button } from '@radix-ui/themes'
+interface issueType{
+  title:string,
+  description:string
+}
 
 const NewIssue = () => {
+  const router=useRouter();
+  const [error,setError]=useState("");
+  const {register,control,handleSubmit}=useForm<issueType>();
+  
   return (
-    <div className='max-w-xl space-y-3'>
+    <div className="max-w-xl">
+      {error && <Callout.Root color='red '>
+        <CalloutText>{error}</CalloutText>
+        </Callout.Root>}
+    <form className=' space-y-3' 
+    onSubmit={handleSubmit( async(data)=>{
+      try {
+        const response=await axios.post('/api/issues',data);
+        router.push("/issues")
+      } catch (error) {
+        setError("A unexpected error has occured")
+      }
+    })}>
         <TextField.Root>
-          <TextField.Input placeholder='Title'></TextField.Input>
+          <TextField.Input placeholder='Title'  {...register('title')}></TextField.Input>
         </TextField.Root>
-        <TextArea placeholder='description'></TextArea>
+        <Controller  name='description' control={control} render={({field})=> <SimpleMDE  placeholder='description' {...field}  />}/>
         <Button>Submit</Button>
-    </div>
+    </form>
+        </div>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import '@radix-ui/themes/styles.css';
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { Theme } from '@radix-ui/themes';
+import { Theme, ThemePanel } from '@radix-ui/themes';
 import NavBar from './NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -20,12 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Theme>
-              <NavBar/>
+      <Theme appearance="light" accentColor="cyan" radius="large" scaling="105%">              <NavBar/>
               <main className='p-5'>
                 {children}
               </main>
-        </Theme>
+          </Theme>
         </body>
     </html>
   )
